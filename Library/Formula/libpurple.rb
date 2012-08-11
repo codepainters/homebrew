04c84da2ca89174fb1fe147226e25731294bba27
Thijs Alkemade <thijsalkemade@gmail.com>
2012-08-11 14:26:34 +0200

    Added a simple test to see if libpurple works.

--- Library/Formula/libpurple.rb
+++ Library/Formula/libpurple.rb
@@ -51,5 +51,74 @@ class Libpurple < Formula
     system "./autogen.sh", *args
 
     system "make install"
+
+    ohai "Updating po files"
+
+    Dir.chdir "po"
+
+    system "make update-po"
+    system "make all"
+    system "make install"
+  end
+
+  def test
+    unless Formula.factory("pkg-config").installed?
+      puts "pkg-config is required to run this test, but is not installed"
+      exit 1
+    end
+
+    mktemp do
+      (Pathname.pwd/'test.c').write <<-EOS.undent
+        #include <string.h>
+        #include <libpurple/eventloop.h>
+
+        guint add_timeout(guint interval, GSourceFunc function, gpointer data)
+        {
+          return 0;
+        }
+
+        guint input_add(gint fd, PurpleInputCondition condition, PurpleInputFunction func, gpointer user_data)
+        {
+          return 0;
+        }
+
+
+        static PurpleEventLoopUiOps eventLoopUiOps = {
+            add_timeout,
+            NULL,
+            input_add,
+            NULL,
+            NULL,
+            NULL,
+            NULL,
+            NULL,
+            NULL
+        };
+
+        int main(void)
+        {
+            purple_eventloop_set_ui_ops(&eventLoopUiOps);
+            return purple_core_init("test") ? 0 : 1;
+        }
+        EOS
+
+      ENV.macosxsdk "10.8"
+      ENV.remove_from_cflags(/ ?-mmacosx-version-min=10\.\d/)
+      ENV['MACOSX_DEPLOYMENT_TARGET'] = "10.6"
+      ENV.append_to_cflags("-mmacosx-version-min=10.6")
+
+      flags = *`pkg-config --cflags --libs purple`.split
+      flags += ENV.cflags.split
+      flags.delete("-arch=x86")
+      flags << "-arch x86_64"
+      system ENV.cc, "-o", "test", "test.c", *flags
+      system "./test"
+
+      flags.delete("-arch=x86")
+      flags << "-arch i386"
+      system ENV.cc, "-o", "test", "test.c", *flags
+
+      system "./test"
+    end
   end
 end
