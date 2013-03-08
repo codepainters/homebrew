8c507bcd6530254e7588f127f971be75afb67656
Thijs Alkemade <thijsalkemade@gmail.com>
2013-03-08 23:16:02 +0100

    Fix universal builds of libgcrypt.
    SIZEOF_UNSIGNED_LONG was incorrectly set to 8 on i386, use a patch from
    macports to make it depend on arch.
    
    Also added a test that shows libgcrypt hanging without this patch.

--- Library/Formula/libgcrypt.rb
+++ Library/Formula/libgcrypt.rb
@@ -32,14 +32,77 @@ class Libgcrypt < Formula
 
     ENV.append_to_cflags("-D_FORTIFY_SOURCE=2")
     ENV.append_to_cflags("-fstack-protector-all")
+    ENV.append_to_cflags("-arch i386 -arch x86_64")
 
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--disable-asm",
                           "--with-gpg-error-prefix=#{HOMEBREW_PREFIX}"
     # Parallel builds work, but only when run as separate steps
+    if build.universal?
+      system "curl 'https://trac.macports.org/export/56608/trunk/dports/devel/libgcrypt/files/config.h.ed' | ed - config.h"
+    end
+
     system "make", "CFLAGS=#{cflags}"
     system "make check"
     system "make install"
+  end
+
+  test do
+    (testpath/'test.c').write <<-EOS.undent
+      #include <gcrypt.h>
+
+      int main(void)
+      {
+          gcry_error_t err;
+
+          if(!gcry_check_version(GCRYPT_VERSION)) {
+            exit(2);
+          }
+
+          gcry_control (GCRYCTL_DISABLE_SECMEM, 0);
+
+          gcry_control (GCRYCTL_INITIALIZATION_FINISHED, 0);
+
+          gcry_sexp_t key, parms, privkey;
+          static const char *parmstr = "(genkey (dsa (nbits 4:1024)))";
+
+          err = gcry_sexp_new(&parms, parmstr, strlen(parmstr), 0);
+
+          if (err) return err;
+
+          err = gcry_pk_genkey(&key, parms);
+
+          if (err) return err;
+
+          gcry_sexp_release(parms);
+
+          privkey = gcry_sexp_find_token(key, "private-key", 0);
+
+          gcry_sexp_release(key);
+
+          return 0;
+      }
+      EOS
+
+    ENV.macosxsdk "10.8"
+    ENV.remove_from_cflags(/ ?-mmacosx-version-min=10\.\d/)
+    ENV['MACOSX_DEPLOYMENT_TARGET'] = "10.6"
+    ENV.append_to_cflags("-mmacosx-version-min=10.6")
+
+    flags = *`#{bin}/libgcrypt-config --cflags --libs`.split
+    flags += ENV.cflags.split
+    flags.delete("-arch=x86")
+    flags.delete("-march=native")
+    flags << "-arch x86_64"
+    system ENV.cc, "-o", "test", "test.c", *flags
+    system "./test"
+
+    flags.delete("-arch x86_64")
+    flags << "-arch i386"
+    flags << "-m32"
+    system ENV.cc, "-o", "test", "test.c", *flags
+
+    system "arch", "-i386", "./test"
   end
 end
